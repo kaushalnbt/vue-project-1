9e76fe1e8686d57927edebd6dc2dbd04ff4fe1f4
Kaushal Jaiswal <kaushalnbt@gmail.com>
2025-02-04 17:45:37 +0530

    Add confirmed name functionality and update name display in Vue app

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             name: 'Kaushal Jaysawal',
             age: 23,
-            imageUrl: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+            imageUrl: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+            confirmedName: '',
         };
     },
     methods: {
@@ -25,6 +26,9 @@ const app = Vue.createApp({
         submitForm() {
             alert('Form Submitted');
         },
+        confirmedNameOutput() {
+            this.confirmedName = this.name;
+        }
     }
 });
 
